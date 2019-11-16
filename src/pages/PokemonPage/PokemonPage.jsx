139f512789c8a5ea150e6f9742d689bda1f48ae5
Borodina <asya07200@gmail.com>
2019-11-16 14:56:24 +0300

    Реализована страница PokemobPage.

--- src/pages/PokemonPage/PokemonPage.jsx
+++ src/pages/PokemonPage/PokemonPage.jsx
@@ -1,13 +1,29 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 
-export function PokemonPage () {
-    const { id } = useParams();
-    
-    return (
-        <div>
-            <p>PokemonPage</p>
-            <h3>ID: {id}</h3>
-        </div>
-    )
+export function PokemonPage() {
+  const { id } = useParams();
+  const [isLoad, setLoad] = useState(false);
+  const [pokemonName, setPokemonName] = useState(null);
+
+  useEffect(() => {
+    setLoad(true);
+
+    fetch(`https://pokeapi.co/api/v2/item/${id}`)
+      .then(response => response.json())
+      .then(({name}) => {
+
+        setPokemonName(name);
+        setLoad(false);
+      });
+  }, []);
+
+  return (
+    <div>
+        {isLoad ? <span>loading...</span> : null}
+      <p>PokemonPage</p>
+      <h3>ID: {id}</h3>
+      <span>name: {pokemonName}</span>
+    </div>
+  );
 }
